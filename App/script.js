1451de24d10a11ee2bd93ade2c2c411727910ec5
Mandu_!908 <158363723+AH-1908@users.noreply.github.com>
2024-07-07 23:36:07 -0500

    subida de pagina wed

--- App/script.js
+++ App/script.js
@@ -1,19 +1,18 @@
-
 function openService(evt, serviceName) {
     // Ocultar todos los elementos con la clase "tabcontent"
-    var tabcontent = document.getElementsByClassName("tabcontent");
-    for (var i = 0; i < tabcontent.length; i++) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
         tabcontent[i].style.display = "none";
     }
 
-    // Remover la clase "active" de todos los elementos con la clase "tablinks"
-    var tablinks = document.getElementsByClassName("tablinks");
-    for (var i = 0; i < tablinks.length; i++) {
+    // Obtener todos los elementos con la clase "tablinks" y remover la clase "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
         tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
 
-    // Mostrar el contenido de la pestaña actual y añadir la clase "active" al botón que abrió la pestaña
+    // Mostrar el contenedor del servicio actual y añadir la clase "active" al botón que abrió el tab
     document.getElementById(serviceName).style.display = "block";
     evt.currentTarget.className += " active";
 }
-
